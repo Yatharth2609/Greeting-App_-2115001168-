a8decb027321fdb9625030539a1cb41e92aff8b6
Yatharth2609 <yatharth.mishra2002@gmail.com>
2025-03-04 19:53:04 +0530

    [Yatharth] Fix: Minor Bugs fixed

---
+++ RepositoryLayer/Entity/GreetingDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace RepositoryLayer.Entity
+{
+    public class GreetingDBContext : DbContext
+    {
+        public GreetingDBContext(DbContextOptions<GreetingDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GreetingModel> Greetings { get; set; }
+    }
+}
