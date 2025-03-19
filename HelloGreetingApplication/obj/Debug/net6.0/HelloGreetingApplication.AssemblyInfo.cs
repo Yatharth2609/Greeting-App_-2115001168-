e0df9801196a7262f7c4f234e612ecedfe46c116
Yatharth2609 <yatharth.mishra2002@gmail.com>
2025-03-19 19:30:51 +0530

    bugfix

--- HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
+++ HelloGreetingApplication/obj/Debug/net6.0/HelloGreetingApplication.AssemblyInfo.cs
@@ -14,27 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8decb027321fdb9625030539a1cb41e92aff8b6")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ef5665f916af49b6512c2ac1057f953e95830e")]
->>>>>>> UC4
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78a9e1dd6ba7ed8ecac2f81a0deaf24d6424be39")]
->>>>>>> UC5
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e66738456d6337898426fc83640ac1ab02b3ccd0")]
->>>>>>> UC6
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+981dc63329f880f5693b0cb3bc382dc000d7e172")]
->>>>>>> UC7
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769f346c92a78c20d37a4de68e88581f52e4aaec")]
->>>>>>> UC8
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b98a5cb23d1987489e123a35c84b8640013288")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloGreetingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
