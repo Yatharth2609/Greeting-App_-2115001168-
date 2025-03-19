9ae3e905bfc7d634ba237f412e424ec88c13b636
Yatharth2609 <yatharth.mishra2002@gmail.com>
2025-03-19 19:32:41 +0530

    fixes

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+769f346c92a78c20d37a4de68e88581f52e4aaec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cadc9861a44e83151d3150d1bcdbc568e8c2c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
